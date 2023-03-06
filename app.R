b56ec848088b1adadc9fb73c4515f8981ed1098a
Gyorgy Barabas <dysordys@umich.edu>
2023-03-06 19:25:39 +0100

    Tried fixing Dropbox communication

--- app.R
+++ app.R
@@ -1,27 +1,46 @@
+# Ideas for stats dashboard
+#
+# What is the distribution of rewards?
+#  * distribution of rewards (total)
+#  * distribution of rewards across neighborhoods (how evenly distributed?)
+#  * profitability by neighborhood (how evenly distributed?)
+#
+# What is the distribution of number of overlays (nodes) per neighborhood?
+#  * distribution of honest revealers per neighbourhood
+#  * distribution of playing and staked nodes per neighbourhood
+#  * distribution of ‘dishonest’ nodes per neighbourhood
+#
+# Other
+#  * In the last x rounds, what was the average number of revealers by round
+#  * Skipped rounds distribution over time
+#  * Distribution of reported depth and are there any seeming anomalies
+#  * How often are 'honest' nodes misreporting their rc
+#  * How many rounds are controversial (have some disagreement in hashes)
+#  * Distribution of freezes over time (how many nodes frozen at any given point)
+#  * Distribution of freezes over neighborhoods
+#
+# Staking
+#  * Distribution of staked nodes not playing
+#  * Distribution of stakes by neighbourhood
+
 library(shiny)
 library(tidyverse)
+library(rdrop2)
 
-source("download_data.R") # Functions to download data
-source("clean_data.R") # Functions to clean up the data
-source("price_model.R") # Functions for tracking price oracle
+source("download_data.R")
+source("clean_data.R")
+source("price_model.R")
+source("dropbox_comm.R")
 
-loadDataFromDropbox <- function(path = "shinystats/data.rds") {
-  rdrop2::drop_download(path, overwrite = TRUE)
-}
 
-saveDataToDropbox <- function(dat) {
-  write_rds(dat, "data.rds", compress = "xz")
-  rdrop2::drop_upload("data.rds", path = "shinystats") # Upload to Dropbox
-}
-
-loadDataFromDropbox() # Initialize data
+loadDataFromDropbox()
 dat <- read_rds("data.rds")
 
 
 
 ui <- fluidPage(
   verticalLayout(
-    selectInput(inputId = "tab", label = "Which table?",
+    selectInput(inputId = "tab", label = "Which data?",
                 choices = c("Revealers", "Skipped rounds")),
     sliderInput(inputId = "ip", label = "Initial price", min = 2^10,
                 max = 2^13, value = 2^10, step = 1, width = "80%"),
@@ -40,16 +59,17 @@ server <- function(input, output) {
   output$outFig <- renderPlot({
     if (input$tab == "Revealers") {
       dat %>%
-        filter(round %in% reduce(input$roundRange, `:`)) %>%
         group_by(round) %>%
         mutate(honest = (id == id[event == "won"])) %>%
         filter(event == "revealed") %>%
         summarise(nHonest = sum(honest)) %>%
         ungroup() %>%
         mutate(price = accumPrice(nHonest, initPrice = input$ip)) %>%
+        filter(round %in% reduce(input$roundRange, `:`)) %>%
         ggplot(aes(x = round, y = price)) +
         geom_line(colour = "steelblue") +
-        theme_bw(base_size = 16)
+        theme_bw(base_size = 16) +
+        theme(plot.margin = unit(c(0.2, 2, 0.2, 0.2), "cm"))
     } else {
       tibble(round = reduce(input$roundRange, `:`)) %>%
         anti_join(distinct(select(dat, round)), by = "round") %>%
@@ -57,20 +77,21 @@ server <- function(input, output) {
         geom_point(colour = "steelblue", alpha = 0.7) +
         theme_bw(base_size = 16) +
         theme(axis.title.y = element_blank(), axis.ticks.y = element_blank(),
-              axis.text.y = element_blank())
+              axis.text.y = element_blank(),
+              plot.margin = unit(c(0.2, 2, 0.2, 0.2), "cm"))
     }
   })
   output$outTab <- renderTable({
     if (input$tab == "Revealers") {
       dat %>%
-        filter(round %in% reduce(input$roundRange, `:`)) %>%
         group_by(round) %>%
         mutate(honest = (id == id[event == "won"])) %>%
         filter(event == "revealed") %>%
         summarise(`number of revealers` = n(),
                   `honest revealers` = sum(honest)) %>%
         ungroup() %>%
-        mutate(price = accumPrice(`honest revealers`, initPrice = input$ip))
+        mutate(price = accumPrice(`honest revealers`, initPrice = input$ip)) %>%
+        filter(round %in% reduce(input$roundRange, `:`))
     } else {
       tibble(round = min(dat$round):max(dat$round)) %>%
         anti_join(distinct(select(dat, round)), by = "round") %>%
---
+++ dropbox_comm.R
@@ -0,0 +1,38 @@
+dropAuthRefresh <- function(new_user = FALSE,
+                            key = "mmhfsybffdom42w",
+                            secret = "l8zeqqqgm1ne5z0",
+                            cache = TRUE,
+                            rdstoken = "droptoken.rds") {
+  if (!new_user & !is.na(rdstoken)) {
+    if (file.exists(rdstoken)) {
+      dropbox_token <- readRDS(rdstoken)
+    } else {
+      stop("Token file not found")
+    }
+  } else {
+    if (new_user && file.exists(".httr-oauth")) {
+      message("Removing old credentials...")
+      file.remove(".httr-oauth")
+    }
+    dropbox <- httr::oauth_endpoint(
+      authorize = "https://www.dropbox.com/oauth2/authorize?token_access_type=offline",
+      access = "https://api.dropbox.com/oauth2/token"
+    )
+    dropbox_app <- httr::oauth_app("dropbox", key, secret)
+    dropbox_token <- httr::oauth2.0_token(dropbox, dropbox_app, cache = cache)
+    if (!inherits(dropbox_token, "Token2.0")) {
+      stop("Something went wrong, try again")
+    }
+  }
+  return(dropbox_token)
+}
+
+loadDataFromDropbox <- function(path = "shinystats/data.rds",
+                                dtoken = dropAuthRefresh()) {
+  drop_download(path, overwrite = TRUE, dtoken = dtoken)
+}
+
+saveDataToDropbox <- function(dat, dtoken = dropAuthRefresh()) {
+  write_rds(dat, "data.rds", compress = "xz")
+  drop_upload("data.rds", path = "shinystats", dtoken = dtoken)
+}

2dda5e70f7b070248ebc8bc7997d9c3fa91fe372
Gyorgy Barabas <dysordys@umich.edu>
2023-05-31 12:45:30 +0200

    update yaml

--- refresh-test.sh
+++ refresh-test.sh
@@ -1,3 +1,3 @@
-aws s3 --profile rw-research-team cp s3://ethswarm-research-team/data-test2.rds data-test2.rds
+aws s3 cp s3://ethswarm-research-team/data-test2.rds data-test2.rds
 Rscript data_refresh.R short
-aws s3 --profile rw-research-team cp data-test2.rds s3://ethswarm-research-team/
+aws s3 cp data-test2.rds s3://ethswarm-research-team/

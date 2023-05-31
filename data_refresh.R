98f94b2b940f88dcdfdfe2763fc2dfcdebd81ca2
Gyorgy Barabas <dysordys@umich.edu>
2023-05-31 14:25:02 +0200

    update yaml

--- data_refresh.R
+++ data_refresh.R
@@ -1,25 +1,25 @@
 clargs <- commandArgs(trailingOnly = TRUE)
-if (length(clargs) > 0) short <- clargs[1] else short <- "long"
+if (length(clargs) > 0) short <- clargs[1] else short <- NA
 
 dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)  # create personal library
 .libPaths(Sys.getenv("R_LIBS_USER"))  # add to the path
-install.packages("tidyverse")
+install.packages("jsonlite")
 
-suppressPackageStartupMessages({
-  library(tidyverse)
-  library(fs)
-})
-
-source("download_clean.R")
-
-if (file_access("data.rds", mode = "write"))
-  minRound <- max(read_rds("data.rds")$roundNumber) else minRound <- 0
-
-if (short == "short")
-  fetch <- function(start, minRound) fetchJson() else fetch <- fetchJsonAll
-
-fetch(minRound = minRound) %>%
-  cleanData() %>%
-  mergeData(read_rds("data.rds") %>% select(-date)) %>%
-  mutate(date = roundsToDate(roundNumber, lubridate::today())) %>%
-  write_rds("data.rds")
+# suppressPackageStartupMessages({
+#   library(tidyverse)
+#   library(fs)
+# })
+#
+# source("download_clean.R")
+#
+# if (file_access("data.rds", mode = "write"))
+#   minRound <- max(read_rds("data.rds")$roundNumber) else minRound <- 0
+#
+# if (short == "short")
+#   fetch <- function(start, minRound) fetchJson() else fetch <- fetchJsonAll
+#
+# fetch(minRound = minRound) %>%
+#   cleanData() %>%
+#   mergeData(read_rds("data.rds") %>% select(-date)) %>%
+#   mutate(date = roundsToDate(roundNumber, lubridate::today())) %>%
+#   write_rds("data.rds")

aad170517f79723225e8d43a85fdeb8b695629a2
Gyorgy Barabas <dysordys@umich.edu>
2023-05-31 12:47:59 +0200

    update yaml

--- refresh-test.sh
+++ refresh-test.sh
@@ -1,3 +1,4 @@
 aws s3 cp s3://ethswarm-research-team/data-test2.rds data-test2.rds
+Rscript -e install.packages("tidyverse")
 Rscript data_refresh.R short
 aws s3 cp data-test2.rds s3://ethswarm-research-team/
